92531f4e296ae6b8db12941442ab547611cb39cc
oreleraki@gmail.com <ghub102938ghub>
2020-06-20 18:54:44 +0300

    Added: eslinter + prettier
    Fix: all linter errors

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "es2020": true
+    },
+    "extends": ["prettier", "eslint:recommended"],
+    "parser": "babel-eslint",
+    "plugins": ["prettier"],
+    "parserOptions": {
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
